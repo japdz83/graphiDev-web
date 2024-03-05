de91a77edb24585a42b889525d346ca386894ce3
Johan Prado <japdz83@gmail.com>
2024-03-05 11:05:26 -0400

    Finalizado el maquetado

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,11 +37,12 @@ const blurHeader = () => {
         : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
     //When the scroll is higher than 350 viewport height, add the
-    this.screenY >= 350 ? scrollUp.classList.add('show-scroll')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
         : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
@@ -69,4 +70,17 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2500,
+    delay: 300,
+    reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__img, .new__data, .care__img, .contact__content, .footer`)
+sr.reveal(`.home__data, .care__list, .contact__img`, { delay: 500 })
+sr.reveal(`.new__card`, { delay: 500, interval: 100 })
+sr.reveal(`.shop__card`, { interval: 100 })
+
 
